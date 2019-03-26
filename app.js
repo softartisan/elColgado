65f94b62fb2eb45553d78990f7acb6181c62c53c
softartisan <slcaniob@gmail.com>
2019-03-25 22:57:57 -0300

    API agregada

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
 
-const game = new Hangman('cat',2);
 const h1Puzzle = document.querySelector('#puzzle');
-const spanGuesses = document.querySelector('#spanGuesses')
+const spanGuesses = document.querySelector('#spanGuesses');
+let game;
 
-h1Puzzle.textContent = game.puzzle;
-spanGuesses.textContent = game.statusMessage;
 
 window.addEventListener('keypress', (e) =>{
     const guess = String.fromCharCode(e.charCode);
-   
     game.makeGuess(guess);
+    render();
+});
+
+
+const render = () => {
     h1Puzzle.textContent = game.puzzle;
     spanGuesses.textContent = game.statusMessage;
+}
 
-    game.calculateStatus();
-});
+const startGame = async () => {
+    const puzzle = await getPuzzleFetch('2');
+    game = new Hangman(puzzle,5);
+    render();
+}
+
+
+document.querySelector('#reset').addEventListener('click',startGame);
+
+startGame();
+
+
+// getPuzzleFetch('2').then((puzzle) => {
+//     console.log(puzzle);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+
+
+
+
+
+
+// getLocationFetch().then((location) => {
+//     console.log(location);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// getCountryFetch('CL').then((countryName) =>{
+//     console.log(countryName);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// getCurrentCountry().then((country) => {
+//     console.log(country.name);
+// }).catch((error) => {
+//     console.log(error);
+// })
+
+
+
---
+++ requests.js
@@ -0,0 +1,12 @@
+
+const getPuzzleFetch = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+    
+    if (response.status === 200) {
+        const data = await response.json();
+        return data.puzzle;
+    } else {
+        throw new Error('Unable to get puzzle');
+    }
+}
+
